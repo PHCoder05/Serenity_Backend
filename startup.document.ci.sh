b719c37d9e21ebc8da8bb5950b97d12820a07312
PHCoder05 <pankajhadole4@gmail.com>
2026-04-26 17:55:22 +0530

    fix: expose CI app logs and increase timeout to 120s

--- startup.document.ci.sh
+++ startup.document.ci.sh
@@ -3,8 +3,8 @@ set -e
 
 /opt/wait-for-it.sh mongo:27017
 npm run seed:run:document
-npm run start:prod > prod.log 2>&1 &
+npm run start:prod &
 /opt/wait-for-it.sh maildev:1080
-/opt/wait-for-it.sh -t 60 localhost:3000
+/opt/wait-for-it.sh -t 120 localhost:3000
 npm run lint
 npm run test:e2e -- --runInBand
--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -4,8 +4,8 @@ set -e
 /opt/wait-for-it.sh postgres:5432
 npm run migration:run
 npm run seed:run:relational
-npm run start:prod > prod.log 2>&1 &
+npm run start:prod &
 /opt/wait-for-it.sh maildev:1080
-/opt/wait-for-it.sh -t 60 localhost:3000
+/opt/wait-for-it.sh -t 120 localhost:3000
 npm run lint
 npm run test:e2e -- --runInBand
